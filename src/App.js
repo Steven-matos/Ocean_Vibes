8b530d67d5bb406f0e4124944762d9e25ba40cf3
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-05-04 09:51:10 -0400

    created routes structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+// components
+import Home from "./component/Home.js";
+import About from "./component/About.js";
+import Service from "./component/Service.js";
+
+export default function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div>
+        <div>
+          <img alt="Ocean Vibes Aquariums logo" />
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/about">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/services">
+            <Service />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          Î
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
