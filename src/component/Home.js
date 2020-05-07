6e11c4eb8045829f1531cdbdaef746286e6fd809
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-05-06 21:03:49 -0400

    created simple home page

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import Nav from "./Nav.js";
+
+import { Button, Icon } from "@material-ui/core";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <Nav />
+        <h1>Ocean Vibes Aquarium LLC</h1>
+      </div>
+      <Button variant="contained" color="primary" endIcon={<Icon>call</Icon>}>
+        Call Us
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
